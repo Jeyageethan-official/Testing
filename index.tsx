279449b0eb65b6717353fd4d329d27971bceaaf1
Jeyageethan-official <jeyageethantechsolutions01@gmail.com>
2025-10-20 14:01:14 +0530

    Delete index.tsx

--- index.tsx
+++
@@ -1,4 +0,0 @@
-// This file is deprecated and has been replaced by index.js.
-// The browser cannot execute TypeScript (.tsx) files directly.
-// The logic has been converted to plain JavaScript in index.js to ensure
-// it runs correctly when hosted on static platforms like GitHub Pages.
